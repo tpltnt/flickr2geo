03de77a36df14ec374e922ed547f4b5faa83e859
tpltnt <tpltnt.github@dropcut.net>
2014-02-03 21:06:24 +0100

    usage added

--- flickr2geo.go
+++ flickr2geo.go
@@ -9,14 +9,18 @@ import (
 )
 
 func main() {
-	res, err := http.Get(os.Args[1])
-	if err != nil {
+     if 2 != len(os.Args) {
+        fmt.Println("usage: " + os.Args[0] + " flickr-url")
+        os.Exit(1)
+     }
+	 res, err := http.Get(os.Args[1])
+	 if err != nil {
+	 	log.Fatal(err)
+	 }
+	 html, err := ioutil.ReadAll(res.Body)
+	 res.Body.Close()
+	 if err != nil {
 		log.Fatal(err)
-	}
-	html, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s\n", html)
+	 }
+	 fmt.Printf("%s\n", html)
 }
